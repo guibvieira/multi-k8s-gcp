4a6e1d8494e1bb30d8b2d9f52ef4046bce8a30f1
guibvieira <guibvieira@gmail.com>
2020-05-17 16:28:47 +0100

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t guivieira/multi-client:latest -t guibvieira/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t guibvieira/multi-server:latest -t guibvieira/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t guibvieira/multi-worker:latest -t guibvieira/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push guibvieira/multi-client:latest
 docker push guibvieira/multi-server:latest
 docker push guibvieira/multi-worker:latest
@@ -11,5 +12,5 @@ docker push guibvieira/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=guibvieira/multi-server:$SHA
-kubectl set image deployments/client-deployment server=guibvieira/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=guibvieira/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=guibvieira/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=guibvieira/multi-worker:$SHA
